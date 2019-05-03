f462cfecc9b871a24df43222e0cdac6405c6dd35
macleanpinto <macleanmauricepinto@gmail.com>
2019-05-03 09:50:48 +0000

    Initial application generated by JHipster-6.0.0

---
+++ gateway/src/main/webapp/app/admin/admin.module.ts
@@ -0,0 +1,43 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { GatewaySharedModule } from 'app/shared';
+/* jhipster-needle-add-admin-module-import - JHipster will add admin modules imports here */
+
+import {
+  adminState,
+  UserMgmtComponent,
+  UserMgmtDetailComponent,
+  UserMgmtUpdateComponent,
+  UserMgmtDeleteDialogComponent,
+  LogsComponent,
+  JhiMetricsMonitoringComponent,
+  JhiHealthModalComponent,
+  JhiHealthCheckComponent,
+  JhiConfigurationComponent,
+  JhiDocsComponent,
+  JhiGatewayComponent
+} from './';
+
+@NgModule({
+  imports: [
+    GatewaySharedModule,
+    RouterModule.forChild(adminState)
+    /* jhipster-needle-add-admin-module - JHipster will add admin modules here */
+  ],
+  declarations: [
+    UserMgmtComponent,
+    UserMgmtDetailComponent,
+    UserMgmtUpdateComponent,
+    UserMgmtDeleteDialogComponent,
+    LogsComponent,
+    JhiConfigurationComponent,
+    JhiHealthCheckComponent,
+    JhiHealthModalComponent,
+    JhiDocsComponent,
+    JhiGatewayComponent,
+    JhiMetricsMonitoringComponent
+  ],
+  entryComponents: [UserMgmtDeleteDialogComponent, JhiHealthModalComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class GatewayAdminModule {}
